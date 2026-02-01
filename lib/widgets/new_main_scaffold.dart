90841b1f0ba0fc0aae543d07af3e1385b3691ead
Ali Mohammad Fawzi <ali@example.com>
2026-02-01 19:18:18 +0100

    add more ios fix

--- lib/widgets/new_main_scaffold.dart
+++ lib/widgets/new_main_scaffold.dart
@@ -37,8 +37,8 @@ class _NewMainScaffoldState extends State<NewMainScaffold> {
 
   Widget _buildDrawer(BuildContext context) {
     return Drawer(
-      width: MediaQuery.of(context).size.width *
-          0.5, // Make drawer 50% of screen width
+      width: MediaQuery.of(context).size.width * 0.5 +
+          30, // Make drawer 50% of screen width + 30 pixels
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
